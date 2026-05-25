c270cba1d6e20f44d36a2e4581d62efd4a9ca547
amtsklar <amtsklar@gmail.com>
2026-05-25 18:33:52 +0200

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// SPA Routing: Pfad nach 404-Redirect wiederherstellen
+const savedPath = sessionStorage.getItem('__amtsklar_path')
+if (savedPath) {
+  sessionStorage.removeItem('__amtsklar_path')
+  window.history.replaceState(null, '', savedPath)
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
